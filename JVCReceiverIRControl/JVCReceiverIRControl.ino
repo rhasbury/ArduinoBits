45107ed486dff942e4592c78ee9e4d6d2b02fc0b
Ryan Hasbury <rhasbury@gmail.com>
2018-03-20 14:05:16 -0400

    Added compulink reciever

--- JVCReceiverIRControl/JVCReceiverIRControl.ino
+++ JVCReceiverIRControl/JVCReceiverIRControl.ino
@@ -1,106 +1,80 @@
-/*
- * IRremote: IRsendDemo - demonstrates sending IR codes with IRsend
- * An IR LED must be connected to Arduino PWM pin 3.
- * Version 0.1 July, 2009
- * Copyright 2009 Ken Shirriff
- * http://arcfn.com
- * JVC and Panasonic protocol added by Kristian Lauszus (Thanks to zenwheel and other people at the original blog post)
+/* send.ino Example sketch for IRLib2
+ *  Illustrates how to send a code.
  */
-#include <IRremote.h>
- 
-#define PanasonicAddress      0x4004     // Panasonic address (Pre data) 
-#define PanasonicPower        0x100BCBD  // Panasonic Power button
+#include <IRLibSendBase.h>    // First include the send base
+//Now include only the protocols you wish to actually use.
+//The lowest numbered protocol should be first but remainder 
+//can be any order.
+#include <IRLibRecv.h>
+#include <IRLibDecodeBase.h>
+#include <IRLib_P06_JVC.h>    
+#include <IRLibCombo.h>     // After all protocols, include this
+// All of the above automatically creates a universal sending
+// class called "IRsend" containing only the protocols you want.
+// Now declare an instance of that sender.
 
-#define JVCPower              0xC5E8
+#define TVUSB 7 //D7
+#define JVCPower 8 //D8
 
-int LED = 13;
-
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
-
-IRsend irsend;
-
-void setup()
-{
-    Serial.begin(9600);
-    pinMode(LED, OUTPUT);
-    inputString.reserve(200);
-    //digitalWrite(LED, HIGH);
-    Serial.println("fired power button");
-    irsend.sendPanasonic(PanasonicAddress,PanasonicPower); // This should turn your TV on and off
-  
-    irsend.sendJVC(JVCPower, 16,0); // hex value, 16 bits, no repeat
-    delayMicroseconds(50); // see http://www.sbprojects.com/knowledge/ir/jvc.php for information
-    irsend.sendJVC(JVCPower, 16,1); // hex value, 16 bits, repeat
-    delay(500);
-//    digitalWrite(LED, LOW);
-//    delay(500); // see http://www.sbprojects.com/knowledge/ir/jvc.php for information
-//    irsend.sendJVC(JVCPower, 16,1); // hex value, 16 bits, repeat
-//    delay(500); // see http://www.sbprojects.com/knowledge/ir/jvc.php for information
-//    irsend.sendJVC(0xC5E8, 16,1); // hex value, 16 bits, repeat
-//    delay(500); // see http://www.sbprojects.com/knowledge/ir/jvc.php for information
-//    irsend.sendJVC(0xC5C0, 16,1); // hex value, 16 bits, repeat
+IRsend mySender;
+IRrecv MyReceiver(11);
+IRdecode MyDecoder;
 
 
+int TVUSB_state = 0;
+int JVCPower_state = 0;
 
-    
+
+void setup() {
+  Serial.begin(9600);
+  delay(2000); while (!Serial); //delay for Leonardo  
+  pinMode(TVUSB, INPUT_PULLUP);
+  pinMode(JVCPower, INPUT_PULLUP);
+  //pinMode(11, INPUT);
+  MyReceiver.enableIRIn();//start receiving
 }
 
 void loop() {
-    while (Serial.available()) {
-      // get the new byte:
-      char inChar = (char)Serial.read();
-      // add it to the inputString:
-      inputString += inChar;
-      // if the incoming character is a newline, set a flag
-      // so the main loop can do something about it:
-      if (inChar == '\n' or inChar == ';') {
-        stringComplete = true;
-      }
-    }
-    if (stringComplete) {
-        if(inputString.indexOf("jvc_power_on") >= 0){
-        irsend.sendJVC(JVCPower, 16,0); // hex value, 16 bits, no repeat
-        delayMicroseconds(50); // see http://www.sbprojects.com/knowledge/ir/jvc.php for information
-        irsend.sendJVC(JVCPower, 16,1); // hex value, 16 bits, repeat
-          digitalWrite(LED, HIGH);
-          Serial.println("turned on");
-          delay(500);
-          digitalWrite(LED, HIGH);
-          delay(500);
-          digitalWrite(LED, LOW);
-          delay(500);
-          digitalWrite(LED, HIGH);
-          delay(500);
-          digitalWrite(LED, LOW);
-          delay(500);
-          digitalWrite(LED, HIGH);
-          delay(500);
-          digitalWrite(LED, LOW);
-          delay(500);
-        }
-  
-        if(inputString.indexOf("jvc_power_off") >= 0){
-          irsend.sendJVC(JVCPower, 16,1); // hex value, 16 bits, no repeat        
-          digitalWrite(LED, HIGH);
-          Serial.println("turned off");
-          delay(500);
-        }
-    
-    
-    digitalWrite(LED, LOW);
-    inputString = "";
-    stringComplete = false;
-    }
-  
-  
-  irsend.sendPanasonic(PanasonicAddress,PanasonicPower); // This should turn your TV on and off
-  
-  irsend.sendJVC(JVCPower, 16,0); // hex value, 16 bits, no repeat
-  delayMicroseconds(50); // see http://www.sbprojects.com/knowledge/ir/jvc.php for information
-  irsend.sendJVC(JVCPower, 16,1); // hex value, 16 bits, repeat
+  /*
+  if (Serial.read() != -1) {
+    //send a code every time a character is received from the 
+    // serial port. You could modify this sketch to send when you
+    // push a button connected to an digital input pin.
+    //Substitute values and protocols in the following statement
+    // for device you have available.
+    mySender.send(JVC, 0xC5E8, 16);//Sony DVD power A8BCA, 20 bits    
+    Serial.println(F("Sent signal."));
+  }
+  */
 
+  JVCPower_state = digitalRead(JVCPower);
+  TVUSB_state = digitalRead(TVUSB);
+
+  if(digitalRead(TVUSB) == HIGH){
+    if(digitalRead(JVCPower) == LOW){
+      mySender.send(JVC, 0xC5E8, 16);  
+    }
+  }
   
-  delay(500);
+  if(digitalRead(TVUSB) == LOW){
+    if(digitalRead(JVCPower) == HIGH){
+      mySender.send(JVC, 0xC5E8, 16);  
+    }
+  }
+  Serial.print("Power: ");
+  Serial.print(JVCPower_state);
+  Serial.print("  TVUSB: ");
+  Serial.print(TVUSB_state);
+  Serial.println("  ");
+
+  if (MyReceiver.getResults()) {//wait till it returns true
+    MyDecoder.decode();
+    MyDecoder.dumpResults();
+    MyReceiver.enableIRIn(); //restart the receiver
+    }
+
+  Serial.println(F("Looping."));
   
+  delay(5000);
 }
+
