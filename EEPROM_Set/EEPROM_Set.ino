70cdf9d74ff24b8062d254e0c7db62767d2ac4bd
Ryan Hasbury <rhasbury@gmail.com>
2018-08-19 12:07:29 -0400

    added commit for esp eeprom

--- EEPROM_Set/EEPROM_Set.ino
+++ EEPROM_Set/EEPROM_Set.ino
@@ -5,17 +5,22 @@
 // first 6 bytes of the EEPROM
 
 #include <EEPROM.h>
-char sID[7] = "PW0002";
+char sID[7] = "NC0002";
 
 
 void setup()
 {
  Serial.begin(9600);
+ EEPROM.begin(6);
  for (int i=0; i<6; i++) {
+   
    EEPROM.write(i,sID[i]);
+   
  }
+ EEPROM.commit();
 }
 
 void loop() {
  Serial.println(sID);
+ delay(1000);
 }
